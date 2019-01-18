061516bcb9a62ae4f5344218928d1ade90a09386
Larry Eisenman <leisenman@wustl.edu>
2019-01-18 10:15:03 -0600

    functions in correct files

--- seeg/source_image.py
+++ seeg/source_image.py
@@ -20,5 +20,174 @@ from . import source_image as srci
 from . import utils
 
 
-class Seeg():
-    pass
+def calc_power(seizure, freqs):
+    n_cycles = freqs
+    n_channels = seizure['seizure']['bipolar'].info['nchan']
+    n_times = seizure['seizure']['bipolar'].n_times
+    data = np.zeros((1, n_channels, n_times))
+    data[0, :, :] = seizure['seizure']['bipolar'].get_data()[:, :]
+    sfreq = seizure['seizure']['bipolar'].info['sfreq']
+    power_fcn = mne.time_frequency.tfr_array_multitaper
+    seizure_power = power_fcn(data, sfreq=sfreq, freqs=freqs,
+                              n_cycles=n_cycles, output='power')
+
+    n_channels = seizure['baseline']['bipolar'].info['nchan']
+    n_times = seizure['baseline']['bipolar'].n_times
+    data = np.zeros((1, n_channels, n_times))
+    data[0, :, :] = seizure['baseline']['bipolar'].get_data()[:, :]
+    baseline_power = power_fcn(data, sfreq=sfreq, freqs=freqs,
+                               n_cycles=n_cycles, output='power')
+    return baseline_power, seizure_power
+
+
+def compress_data(data, old_freq, new_freq):
+    ''' average points to compress
+    assumes data is 2-D '''
+    num = round(0.5+(data.shape[1]*(new_freq/old_freq)))
+    new = np.zeros((data.shape[0], num))
+    ratio = old_freq/new_freq
+    for i in range(num):
+        start = int(i*ratio)
+        end = int((i+1)*ratio)
+        new[:, i] = np.mean(data[:, start:end], 1)
+
+    return new
+
+
+def ave_power_over_freq_band(seizure, freqs, low=120, high=200):
+    ''' returns the average power between low and high '''
+    baseline_power = seizure['baseline']['power']
+    shape = baseline_power.shape
+    baseline_ave_power = np.zeros((shape[1], shape[3]))
+    seizure_power = seizure['seizure']['power']
+    shape = seizure_power.shape
+    seizure_ave_power = np.zeros((shape[1], shape[3]))
+    freq_index = np.where(np.logical_and(freqs >= low, freqs <= high))
+
+    for i in range(len(seizure['seizure']['bipolar'].ch_names)):
+        baseline_ave_power[i, :] = \
+            np.mean(baseline_power[0, i, freq_index, :], 1)
+        seizure_ave_power[i, :] = \
+            np.mean(seizure_power[0, i, freq_index, :], 1)
+
+    return baseline_ave_power, seizure_ave_power
+
+
+def plot_ave_power(seizure, channel='g7-g8'):
+    plt.figure()
+    b_times = seizure['baseline']['bipolar'].times
+    s_times = seizure['seizure']['bipolar'].times
+    index = seizure['baseline']['bipolar'].ch_names.index(channel)
+    plt.plot(b_times, seizure['baseline']['ave_power'][index, :],
+             s_times, seizure['seizure']['ave_power'][index, :])
+
+
+def extract_power(seizure, D=3, dt=0.2, start=0):
+    assert int(D/dt)*dt == D
+    sfreq = seizure['seizure']['bipolar'].info['sfreq']
+    num_steps = int(D/dt)
+    tstep0 = int((sfreq * start) + 1)
+    baseline_ave_power = seizure['baseline']['ave_power']
+    baseline_ex_power = np.zeros((baseline_ave_power.shape[0], num_steps))
+    seizure_ave_power = seizure['seizure']['ave_power']
+    seizure_ex_power = np.zeros((seizure_ave_power.shape[0], num_steps))
+    for i in range(num_steps):
+        t1 = int(i*dt*sfreq)
+        t2 = int((i+1)*dt*sfreq)
+        baseline_ex_power[:, i] = np.mean(baseline_ave_power[:, t1:t2], 1)
+        t1 += tstep0
+        t2 += tstep0
+        seizure_ex_power[:, i] = np.mean(seizure_ave_power[:, t1:t2], 1)
+
+    return baseline_ex_power, seizure_ex_power
+
+
+def create_volumes(seizure):
+    mri = r'C:\Users\eisenmanl\Documents\brainstorm_data_files'
+    mri += r'\tutorial_epimap\anat\MRI\3DT1pre_deface.nii'
+    img = nib.load(mri)
+    shape = np.round(np.asarray(img.shape)/3).astype(np.int)
+    shape = np.append(shape, seizure['baseline']['ex_power'].shape[-1])
+    affine = img.affine.copy()
+    affine[:3, :3] = img.affine[:3, :3] * 3
+    baseline = np.zeros(shape)
+    baseline_img = nib.Nifti1Image(baseline, affine)
+
+    shape[-1] = seizure['seizure']['ex_power'].shape[-1]
+    seiz = np.zeros(shape)
+    seizure_img = nib.Nifti1Image(seiz, affine)
+    return baseline_img, seizure_img
+
+
+def voxel_coords(mri_coords, inverse):
+    coords = apply_affine(inverse, mri_coords)
+    return coords.astype(np.int)
+
+
+def map_seeg_data(seizure, montage):
+    base_img, seiz_img = create_volumes(seizure)
+    base_data = base_img.get_data()
+    seiz_data = seiz_img.get_data()
+    affine = seiz_img.affine
+    inverse = npl.inv(affine)
+    electrodes = seizure['electrodes']
+    eeg = seizure['baseline']['eeg']
+    bads = eeg.info['bads']
+    coord_list = dict()
+    contact_num = 0
+    for electrode in electrodes:
+        contacts = [i for i in eeg.ch_names if i.startswith(electrode)]
+        num_contacts = utils.find_num_contacts(contacts, electrode)
+        for i in range(1, num_contacts):
+            anode = electrode + str(i)
+            cathode = electrode + str(i+1)
+            if (anode in contacts) and (cathode in contacts):
+                if not ((anode in bads) or (cathode in bads)):
+                    loc1 = montage.dig_ch_pos[anode]*1000
+                    loc2 = montage.dig_ch_pos[cathode]*1000
+                    coord_list[contact_num] = (loc1 + loc2)/2
+                    contact_num += 1
+
+    base_ex = seizure['baseline']['ex_power']
+    seiz_ex = seizure['seizure']['ex_power']
+    for i in coord_list.keys():
+        x, y, z = voxel_coords(coord_list[i], inverse)
+        base_data[x, y, z, :] = base_ex[i, :]
+        seiz_data[x, y, z, :] = seiz_ex[i, :]
+
+    base_img = nilearn.image.smooth_img(base_img, 6)
+    seiz_img = nilearn.image.smooth_img(seiz_img, 6)
+    return base_img, seiz_img
+
+
+def create_source_image(seizure, mri, freqs, montage, low_freq=120,
+                        high_freq=200, seiz_delay=0):
+    ''' create and display the SEEG source image as per David et. al. 2011'''
+    seizure['baseline']['eeg'], seizure['seizure']['eeg'] = \
+        utils.clip_eeg(seizure)
+    seizure['baseline']['bipolar'], seizure['seizure']['bipolar'] = \
+        utils.create_bipolar(seizure)
+    seizure['baseline']['power'], seizure['seizure']['power'] = \
+        calc_power(seizure, freqs)
+    seizure['baseline']['ave_power'], seizure['seizure']['ave_power'] = \
+        ave_power_over_freq_band(seizure, freqs, low=low_freq, high=high_freq)
+    seizure['baseline']['ex_power'], seizure['seizure']['ex_power'] = \
+        extract_power(seizure, start=seiz_delay)
+    seizure['baseline']['img'], seizure['seizure']['img'] = \
+        map_seeg_data(seizure, montage)
+    base_img = seizure['baseline']['img']
+    seiz_img = seizure['seizure']['img']
+
+    nifti_masker = NiftiMasker(memory='nilearn_cache', memory_level=1)
+    base_masked = nifti_masker.fit_transform(base_img)
+    seiz_masked = nifti_masker.fit_transform(seiz_img)
+    data = np.concatenate((base_masked, seiz_masked))
+    labels = np.zeros(30, dtype=np.int)
+    labels[15:] = 1
+    __, t_scores, _ = permuted_ols(
+        labels, data,
+        # + intercept as a covariate by default
+        n_perm=10000, two_sided_test=True,
+        n_jobs=2)  # can be changed to use more CPUs
+
+    return nifti_masker.inverse_transform(t_scores)
