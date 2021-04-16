93a4238cd5c1010064d1da2e2088ea0e531e590f
Larry Eisenman <leisenman@wustl.edu>
2021-04-16 16:37:41 -0500

    add ll seizure detection

--- seeg/line_length.py
+++ seeg/line_length.py
@@ -7,14 +7,19 @@ import mne
 import numpy as np
 
 
-def line_length(raw):
+def line_length(raw, window=1, step=0.25):
     '''
-    calculate line length in 1 second windows with 75% overlap
+    calculate line length is segments of width window with time steps
+    of time step
 
     Parameters
     ----------
     raw : MNE Raw
         EEG data
+    window : float
+        window duration in seconds
+    step : float
+        step in seconds
 
     Returns
     -------
@@ -24,28 +29,58 @@ def line_length(raw):
         standard deviation of the line length in the first interval
     '''
     data = raw.get_data()
-    window = int(raw.info['sfreq'])
-    step = int(0.25*window)
-    time_points = 4*(int(raw.n_times/window) - 1) + 1
-    if raw.n_times%window > 0:
-        print('uneven')
-        time_points += np.ceil(4*(raw.n_times%window))
-        print(f'time_pointes = {time_points}')
-
-    line_len = np.zeros((data.shape[0], time_points))
+    window_pnts = int(raw.info['sfreq']*window)
+    step_pnts = int(step*raw.info['sfreq'])
+    time_pnts = int(1+np.ceil((raw.n_times-window_pnts)/step_pnts))
+    line_len = np.zeros((data.shape[0], time_pnts))
     ll = np.abs(np.diff(data))
-    sd1 = np.std(ll[:, :window], axis=-1)
-    idx = 0
-    start = 0
-    end = start + window
-    while end < ll.shape[-1]:
+    sd1 = np.std(ll[:, :window_pnts], axis=-1)
+    for idx in range(time_pnts-1):
+        start = idx*step_pnts
+        end = start+window_pnts
         line_len[:, idx] = np.mean(ll[:, start:end], axis=-1)
-        idx += 1
-        start += step
-        end += step
 
-    print(idx, start, end)
-    if start < ll.shape[-1]-1:
-        line_len[:, idx] = np.mean(ll[:, start:], axis=-1)
+    idx = time_pnts-1
+    start = idx*step_pnts
+    line_len[:, idx] = np.mean(ll[:, start:], axis=-1)
 
     return line_len, sd1
+
+
+def ll_detect_seizure(raw, window=1, step=0.25, threshold=1):
+    '''
+    Find the seizure onset in each channel defined as the first time that
+    line length is threshold standard deviations above the sd of the first
+    segment
+
+    Parameters
+    ----------
+    raw : MNE Raw
+        EEG data
+    window : float
+        window duration in seconds
+    step : float
+        step in seconds
+    threshold: float
+        number of standard deviations to use for the seizure threshold
+
+    Returns
+    -------
+    ndarray
+        seizure onset time for each channel of raw
+    ndarray
+        line length values for each channel
+    ndarray
+        standard deviation of the line length in the first interval
+    '''
+    ll, sd = line_length(raw, window, step)
+    thresholds = sd*threshold + ll[:, 0]
+    sz = np.zeros_like(thresholds)
+    for i, thresh in enumerate(thresholds):
+        test = np.where(ll[i, :] > thresh)[0]
+        if len(test) > 0:
+            sz[i] = window/2 + (step*test[0])
+        else:
+            sz[i] = np.nan
+
+    return sz + raw.times[0], ll, sd
