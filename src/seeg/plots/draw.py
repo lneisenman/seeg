c5477b5bfd6a240e50e9d6dc4c6caaad94528803
Larry Eisenman <lneisenman@hotmail.com>
2022-04-20 10:20:42 -0500

    delete old vtk code

--- src/seeg/plots/draw.py
+++ src/seeg/plots/draw.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-""" Code to draw spheres, cylinders and cubes in Mayavi and Pyvista figures
+""" Code to draw spheres, cylinders and cubes in Pyvista figures
 
 """
 
@@ -10,70 +10,17 @@ import vtk
 def draw_sphere(plotter, center, radius, color, opacity):
     sphere = pv.Sphere(radius=radius, center=center)
     plotter.add_mesh(sphere, opacity=opacity, color=color)
-    # sphereSource = vtk.vtkSphereSource()
-    # sphereSource.SetCenter(*center)
-    # sphereSource.SetRadius(radius)
-
-    # sphereMapper = vtk.vtkPolyDataMapper()
-    # sphereMapper.SetInputConnection(sphereSource.GetOutputPort())
-
-    # sphereActor = vtk.vtkActor()
-    # sphereActor.SetMapper(sphereMapper)
-    # sphereActor.GetProperty().SetColor(*color)
-    # sphereActor.GetProperty().SetOpacity(opacity)
-
-    # scene.add_actor(sphereActor)
 
 
 def draw_cyl(plotter, tip, base, diam, color, opacity):
     line = pv.Line(tip, base)
     tube = line.tube(radius=diam/2)
     plotter.add_mesh(tube, opacity=opacity, color=color)
-    # lineSource = vtk.vtkLineSource()
-    # lineSource.SetPoint1(*tip)
-    # lineSource.SetPoint2(*base)
-    # lineMapper = vtk.vtkPolyDataMapper()
-    # lineMapper.SetInputConnection(lineSource.GetOutputPort())
-
-    # lineActor = vtk.vtkActor()
-    # lineActor.SetMapper(lineMapper)
-    # lineActor.GetProperty().SetOpacity(0)
-
-    # tubeFilter = vtk.vtkTubeFilter()
-    # tubeFilter.SetInputConnection(lineSource.GetOutputPort())
-    # tubeFilter.SetRadius(diam/2)
-    # tubeFilter.SetNumberOfSides(50)
-    # tubeFilter.Update()
-
-    # tubeMapper = vtk.vtkPolyDataMapper()
-    # tubeMapper.SetInputConnection(tubeFilter.GetOutputPort())
-
-    # tubeActor = vtk.vtkActor()
-    # tubeActor.SetMapper(tubeMapper)
-    # tubeActor.GetProperty().SetColor(*color)
-    # tubeActor.GetProperty().SetOpacity(opacity)
-
-    # scene.add_actor(tubeActor)
 
 
 def draw_cube(plotter, center, x_len, y_len, z_len, color, opacity):
     cube = pv.Cube(center, x_len, y_len, z_len)
     plotter.add_mesh(cube, opacity=opacity, color=color)
-    # cubeSource = vtk.vtkCubeSource()
-    # cubeSource.SetCenter(center)
-    # cubeSource.SetXLength(x_len)
-    # cubeSource.SetYLength(y_len)
-    # cubeSource.SetZLength(z_len)
-
-    # cubeMapper = vtk.vtkPolyDataMapper()
-    # cubeMapper.SetInputConnection(cubeSource.GetOutputPort())
-
-    # cubeActor = vtk.vtkActor()
-    # cubeActor.SetMapper(cubeMapper)
-    # cubeActor.GetProperty().SetColor(*color)
-    # cubeActor.GetProperty().SetOpacity(opacity)
-
-    # scene.add_actor(cubeActor)
 
 
 def draw_text(plotter, text, location, size, color):
