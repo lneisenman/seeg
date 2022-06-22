1fd1a45d6abc5af32f22ea14b9a66dc850629380
Larry Eisenman <lneisenman@hotmail.com>
2022-06-22 17:14:25 -0500

    simplify display4d tests

--- tests/test_display4d.py
+++ tests/test_display4d.py
@@ -9,20 +9,20 @@ import pytest
 import seeg
 
 
-def test1(subject_id, subjects_dir):
-    locations = np.asarray([[25.8315, -9.93608, 8.57813],
-                            [29.30372216, -9.53925532, 8.76840202],
-                            [32.77594432, -9.14243064, 8.95867404],
-                            [36.24816648, -8.74560596, 9.14894606],
-                            [39.72038864, -8.34878128, 9.33921808]])
-    depth = seeg.Depth('A', 5, locations, contact_len=2, spacing=1.5)
-    brain = seeg.create_depths_plot([depth], subject_id, subjects_dir)
-    data = np.ones(15).reshape((5, 3))
-    data[0, 0] = 2
-    data[1, 1] = 2
-    data[2, 2] = 2
-    display = seeg.Display4D(brain, [depth], data)
-    display.show()
+# def test1(subject_id, subjects_dir):
+#     locations = np.asarray([[25.8315, -9.93608, 8.57813],
+#                             [29.30372216, -9.53925532, 8.76840202],
+#                             [32.77594432, -9.14243064, 8.95867404],
+#                             [36.24816648, -8.74560596, 9.14894606],
+#                             [39.72038864, -8.34878128, 9.33921808]])
+#     depth = seeg.Depth('A', 5, locations, contact_len=2, spacing=1.5)
+#     brain = seeg.create_depths_plot([depth], subject_id, subjects_dir)
+#     data = np.ones(15).reshape((5, 3))
+#     data[0, 0] = 2
+#     data[1, 1] = 2
+#     data[2, 2] = 2
+#     display = seeg.Display4D(brain, [depth], data)
+#     display.show()
 
 
 def test_threshold(subject_id, subjects_dir):
@@ -63,22 +63,22 @@ def test_show_depth_values(subject_id, subjects_dir, electrode_names,
     display.show()
 
 
-def test1_BP(subject_id, subjects_dir, T_x_inv):
-    locations = np.asarray([[25.8315, -9.93608, 8.57813],
-                            [29.30372216, -9.53925532, 8.76840202],
-                            [32.77594432, -9.14243064, 8.95867404],
-                            [36.24816648, -8.74560596, 9.14894606],
-                            [39.72038864, -8.34878128, 9.33921808]])
-    data = np.ones(12).reshape((4, 3))
-    data[0, 0] = 2
-    data[1, 1] = 2
-    data[2, 2] = 2
-    depth = seeg.Depth('A', 5, locations, contact_len=2, spacing=1.5)
-    brain = seeg.create_depths_plot([depth], subject_id, subjects_dir)
-    seeg.show_depth_bipolar_values([depth], brain.plotter, data[:, 0],
-                                   affine=T_x_inv)
-    display = seeg.Display4DBP(brain, [depth], data)
-    display.show()
+# def test1_BP(subject_id, subjects_dir, T_x_inv):
+#     locations = np.asarray([[25.8315, -9.93608, 8.57813],
+#                             [29.30372216, -9.53925532, 8.76840202],
+#                             [32.77594432, -9.14243064, 8.95867404],
+#                             [36.24816648, -8.74560596, 9.14894606],
+#                             [39.72038864, -8.34878128, 9.33921808]])
+#     data = np.ones(12).reshape((4, 3))
+#     data[0, 0] = 2
+#     data[1, 1] = 2
+#     data[2, 2] = 2
+#     depth = seeg.Depth('A', 5, locations, contact_len=2, spacing=1.5)
+#     brain = seeg.create_depths_plot([depth], subject_id, subjects_dir)
+#     seeg.show_depth_bipolar_values([depth], brain.plotter, data[:, 0],
+#                                    affine=T_x_inv)
+#     display = seeg.Display4DBP(brain, [depth], data)
+#     display.show()
 
 
 def test_show_depth_values_BP(subject_id, subjects_dir, electrode_names,
