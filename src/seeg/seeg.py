4a0e7528b18bde1a6e3ce03ec3983b75d8241129
Larry Eisenman <lneisenman@hotmail.com>
2022-07-06 15:36:10 -0500

    convert SEEG to dataclass

--- src/seeg/seeg.py
+++ src/seeg/seeg.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from dataclasses import dataclass, field
 import os
 from typing import Sequence, Tuple
 
@@ -8,50 +9,34 @@ from .plots.epi_image import EpiImage
 from .eeg import load_eeg_data
 
 
+@dataclass
 class Seeg():
-    """ Class to contain SEEG recording information
+    """ Class to contain SEEG recording information    """
+    subject: str
+    subjects_dir: str
+    electrode_names: list = field(default_factory=list)
+    bads: list = field(default_factory=list)
+    electrode_file: str = 'recon.fcsv'
+    seiz_delay: float = 0
 
-    Parameters
-    ----------
-    subject : string
-        name of subect folder in Freesurfer subjects_dir
-    subjects_dir : string
-        path to Freesurfer subjects_dir
-    electrode_names : list of strings, optional
-        names of electrodes, by default None
-    bads : list of strings, optional
-        names of bad channels, by default None
-    electrode_file : string
-        electrode data file name
-
-    """
-
-    def __init__(self, subject: str, subjects_dir: str, electrode_names: list,
-                 bads: list, seizure: int = 1,
-                 electrode_file: str = 'recon.fcsv', seiz_delay: float = 0):
-        self.subject = subject
-        self.subjects_dir = subjects_dir
-        self.electrode_names = electrode_names
-        self.bads = bads
-        self.subject_path = os.path.join(subjects_dir, subject)
+    def __post_init__(self) -> None:
+        self.subject_path = os.path.join(self.subjects_dir, self.subject)
         self.eeg_path = os.path.join(self.subject_path, 'eeg')
         self.mri_file = os.path.join(self.subject_path, 'mri/orig.mgz')
-        self.electrode_file = electrode_file
-        self.eeg = load_eeg_data(self.eeg_path, self.electrode_names,
-                                 self.bads, seizure=seizure,
-                                 electrode_file=electrode_file)[seizure-1]
-        self.seiz_delay = seiz_delay
-        # self.contacts = read_electrode_file(self.electrode_file)
+        self.eeg_list = load_eeg_data(self.eeg_path, self.electrode_names,
+                                      self.bads, seizure=0,
+                                      electrode_file=self.electrode_file)
 
     def create_epi_image_map(self, low_freq: float, high_freq: float,
-                             D: float = 3, dt: float = 0.2,
+                             seizure: int = 1, D: float = 3, dt: float = 0.2,
                              method: str = 'welch') -> None:
         """ calculate the source image map analagous to David et al 2011
             and the Brainstorm tutorial
 
         """
-        self.epi_image = EpiImage(self.eeg, self.mri_file, low_freq, high_freq,
-                                  self.seiz_delay, method, D, dt)
+        idx = seizure-1
+        self.epi_image = EpiImage(self.eeg_list[idx], self.mri_file, low_freq,
+                                  high_freq, self.seiz_delay, method, D, dt)
         # self.t_map = create_epi_image_map(self.eeg, self.mri_file,
         #                                   low_freq, high_freq,
         #                                   self.eeg.seiz_delay)
@@ -69,6 +54,6 @@ class Seeg():
         """ calculate the epileptogenicity index as per Bartolomi et al 2008
 
         """
-        self.EI = calculate_EI(self.eeg['seizure']['raw'], freqs,
+        self.EI = calculate_EI(self.eeg_list[0]['seizure']['raw'], freqs,
                                bias=bias, threshold=threshold,
                                tau=tau, H=H)
