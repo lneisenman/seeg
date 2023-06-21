dc05223f871f6048112f87dcb9e5a3f4d3477479
Larry Eisenman <lneisenman@hotmail.com>
2023-06-21 13:25:39 -0500

    skip napari test

--- tests/test_napari.py
+++ tests/test_napari.py
@@ -4,10 +4,12 @@ import os
 
 import mne
 import numpy as np
+import pytest
 
 import seeg
 from seeg.utils import map_colors
 
 
+@pytest.mark.skip()
 def test_napari(t_map, mri):
     seeg.plot_3d_epi_image_map(t_map, mri)
